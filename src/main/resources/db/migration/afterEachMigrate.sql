f7af43faef338f44525dc3fd55e5f1a6f8a62da2
Rob Catling <github@rpc-soft.com>
2021-06-16 16:19:47 +0100

    SOCINT-19 change user and database name to match DEV cloud SQL setup, also show placeholder example

--- src/main/resources/db/migration/afterEachMigrate.sql
+++ src/main/resources/db/migration/afterEachMigrate.sql
@@ -4,8 +4,8 @@
 -- Since the CCSvc user does not own the schema (for security), we grant CRUD privileges
 -- after each migration.
 --
-GRANT USAGE ON SCHEMA cc_schema TO ccsvc;
-GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA cc_schema TO ccsvc;
-GRANT SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA cc_schema TO ccsvc;
+GRANT USAGE ON SCHEMA cc_schema TO ${username};
+GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA cc_schema TO ${username};
+GRANT SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA cc_schema TO ${username};
 
 -- EOF
