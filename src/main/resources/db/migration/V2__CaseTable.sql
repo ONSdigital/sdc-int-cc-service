e5169166bced373a1e35dfc79adef540807efbbb
Rob Catling <github@rpc-soft.com>
2021-07-12 11:11:28 +0100

    SOCINT-41 tidy whitespace in flyway file

--- src/main/resources/db/migration/V2__CaseTable.sql
+++ src/main/resources/db/migration/V2__CaseTable.sql
@@ -13,29 +13,29 @@ CREATE TABLE caze (
     actionable_from VARCHAR(255),
     hand_delivery BOOLEAN DEFAULT false,
     address_invalid BOOLEAN DEFAULT false NOT NULL,
-	ce_expected_capacity INT4,
+    ce_expected_capacity INT4,
 
-	-- contact
-	title VARCHAR(255),
-  	forename VARCHAR(255),
-  	surname VARCHAR(255),
-  	tel_no VARCHAR(255),
+    -- contact
+    title VARCHAR(255),
+    forename VARCHAR(255),
+    surname VARCHAR(255),
+    tel_no VARCHAR(255),
 
-  	-- address
+    -- address
     uprn VARCHAR(255),
     address_line1 VARCHAR(255),
     address_line2 VARCHAR(255),
     address_line3 VARCHAR(255),
     town_name VARCHAR(255),
     postcode VARCHAR(255),
-	region VARCHAR(255),
-	estab_type VARCHAR(255),
-	organisation_name VARCHAR(255),
-	latitude VARCHAR(255),
+    region VARCHAR(255),
+    estab_type VARCHAR(255),
+    organisation_name VARCHAR(255),
+    latitude VARCHAR(255),
     longitude VARCHAR(255),
     estab_uprn VARCHAR(255),
     address_type VARCHAR(255),
-	address_level VARCHAR(255),
+    address_level VARCHAR(255),
     --
     created_date_time TIMESTAMP WITH TIME ZONE,
 
