fb1b63c220f6a63d0851da956355064d93244339
SteveScorfield <steve.scorfield@ons.gov.uk>
2021-10-27 11:01:56 +0100

    Remove commented out channel and removed redundant mockito extension

--- src/test/java/uk/gov/ons/ctp/integration/contactcentresvc/endpoint/VersionEndpointTest.java
+++ src/test/java/uk/gov/ons/ctp/integration/contactcentresvc/endpoint/VersionEndpointTest.java
@@ -8,8 +8,6 @@ import static uk.gov.ons.ctp.common.utility.MockMvcControllerAdviceHelper.mockAd
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -17,7 +15,6 @@ import uk.gov.ons.ctp.common.error.RestExceptionHandler;
 import uk.gov.ons.ctp.common.jackson.CustomObjectMapper;
 
 /** Contact Centre Data endpoint Unit test. */
-@ExtendWith(MockitoExtension.class)
 public final class VersionEndpointTest {
   private VersionEndpoint versionEndpoint = new VersionEndpoint();
 
