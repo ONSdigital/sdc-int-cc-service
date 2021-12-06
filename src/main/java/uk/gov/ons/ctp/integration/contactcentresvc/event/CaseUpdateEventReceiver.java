b8a658277a7008b1e2420871580b8226e884f9d3
KieranWardle <23309814+KieranWardle@users.noreply.github.com>
2021-12-06 10:18:23 +0000

    SOCINT-261 Added missing files

---
+++ src/main/java/uk/gov/ons/ctp/integration/contactcentresvc/event/CaseUpdateEventReceiver.java
@@ -0,0 +1,76 @@
+package uk.gov.ons.ctp.integration.contactcentresvc.event;
+
+import static uk.gov.ons.ctp.common.log.ScopedStructuredArguments.kv;
+
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import ma.glasnost.orika.MapperFacade;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.transaction.annotation.Transactional;
+import uk.gov.ons.ctp.common.event.model.CaseEvent;
+import uk.gov.ons.ctp.common.event.model.CaseUpdate;
+import uk.gov.ons.ctp.integration.contactcentresvc.model.Case;
+import uk.gov.ons.ctp.integration.contactcentresvc.repository.db.CaseRepository;
+import uk.gov.ons.ctp.integration.contactcentresvc.repository.db.CollectionExerciseRepository;
+import uk.gov.ons.ctp.integration.contactcentresvc.repository.db.SurveyRepository;
+
+/**
+ * Service implementation responsible for receipt of Case Events. See Spring Integration flow for
+ * details of inbound queue.
+ */
+@Slf4j
+@MessageEndpoint
+public class CaseUpdateEventReceiver {
+  private CaseRepository caseRepo;
+  private MapperFacade mapper;
+  private EventFilter eventFilter;
+
+  public CaseUpdateEventReceiver(
+      CaseRepository caseRepo,
+      SurveyRepository surveyRepo,
+      CollectionExerciseRepository collExRepo,
+      MapperFacade mapper,
+      EventFilter eventFilter) {
+    this.caseRepo = caseRepo;
+    this.mapper = mapper;
+    this.eventFilter = eventFilter;
+  }
+
+  /**
+   * Message end point for events from Response Management.
+   *
+   * @param caseEvent CaseEvent message from Response Management
+   */
+  @ServiceActivator(inputChannel = "acceptCaseEvent")
+  @Transactional
+  public void acceptEvent(CaseEvent caseEvent) {
+
+    CaseUpdate caseUpdate = caseEvent.getPayload().getCaseUpdate();
+    UUID caseMessageId = caseEvent.getHeader().getMessageId();
+
+    log.info(
+        "Entering acceptCaseEvent {}, {}",
+        kv("messageId", caseMessageId),
+        kv("caseId", caseUpdate.getCaseId()));
+
+    if (eventFilter.isValidEvent(
+        caseUpdate.getSurveyId(),
+        caseUpdate.getCollectionExerciseId(),
+        caseUpdate.getCaseId(),
+        caseMessageId.toString())) {
+      try {
+        Case caze = mapper.map(caseUpdate, Case.class);
+        caseRepo.save(caze);
+
+        log.info(
+            "Successful saved Case to database {}, {}",
+            kv("messageId", caseMessageId),
+            kv("caseId", caseUpdate.getCaseId()));
+      } catch (Exception e) {
+        log.error("Case Event processing failed", kv("messageId", caseMessageId), e);
+        throw e;
+      }
+    }
+  }
+}
---
+++ src/test/java/uk/gov/ons/ctp/integration/contactcentresvc/event/SurveyUpdateEventReceiverTest.java
@@ -0,0 +1,68 @@
+package uk.gov.ons.ctp.integration.contactcentresvc.event;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.UUID;
+import javax.persistence.PersistenceException;
+import ma.glasnost.orika.MapperFacade;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.ons.ctp.common.FixtureHelper;
+import uk.gov.ons.ctp.common.event.model.SurveyUpdate;
+import uk.gov.ons.ctp.common.event.model.SurveyUpdateEvent;
+import uk.gov.ons.ctp.integration.contactcentresvc.CCSvcBeanMapper;
+import uk.gov.ons.ctp.integration.contactcentresvc.model.Survey;
+import uk.gov.ons.ctp.integration.contactcentresvc.repository.db.SurveyRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class SurveyUpdateEventReceiverTest {
+
+  @Mock private SurveyRepository repo;
+  @Spy private MapperFacade mapper = new CCSvcBeanMapper();
+
+  @InjectMocks private SurveyUpdateEventReceiver target;
+
+  @Captor private ArgumentCaptor<Survey> surveyCaptor;
+
+  private SurveyUpdateEvent event;
+
+  @BeforeEach
+  public void setup() {
+    event = FixtureHelper.loadPackageFixtures(SurveyUpdateEvent[].class).get(0);
+  }
+
+  @Test
+  public void shouldReceiveSurveyUpdateEvent() {
+    target.acceptEvent(event);
+
+    verify(repo).save(surveyCaptor.capture());
+
+    SurveyUpdate payload = event.getPayload().getSurveyUpdate();
+    Survey survey = surveyCaptor.getValue();
+    verifyMappedSurvey(survey, payload);
+  }
+
+  @Test
+  public void shouldRejectFailingSave() {
+    when(repo.save(any())).thenThrow(PersistenceException.class);
+    assertThrows(PersistenceException.class, () -> target.acceptEvent(event));
+  }
+
+  private void verifyMappedSurvey(Survey survey, SurveyUpdate surveyUpdate) {
+    assertEquals(UUID.fromString(surveyUpdate.getSurveyId()), survey.getId());
+    assertEquals(surveyUpdate.getName(), survey.getName());
+    assertEquals(surveyUpdate.getSampleDefinitionUrl(), survey.getSampleDefinitionUrl());
+    assertEquals(surveyUpdate.getSampleDefinition(), survey.getSampleDefinition());
+  }
+}
