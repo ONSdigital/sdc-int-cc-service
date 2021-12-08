27dd7f2702434e62147f99c98b229d9483f853a8
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2021-12-08 09:36:01 +0000

    SOCINT-228 Tweak to V11 sql

--- src/main/resources/db/migration/V11__ProductsForSurvey.sql
+++ src/main/resources/db/migration/V11__ProductsForSurvey.sql
@@ -1,15 +1,13 @@
 -- add a table to hold products for a survey.
 
 CREATE TABLE product (
-  id UUID NOT NULL,
+  id UUID PRIMARY KEY,
   survey_id UUID REFERENCES survey NOT NULL,
   delivery_channel TEXT NOT NULL,
   product_group TEXT,
   pack_code TEXT NOT NULL,
   description TEXT NOT NULL,
-  metadata JSONB,
-  
-  PRIMARY KEY (id)
+  metadata JSONB
 );
 
 ALTER TABLE survey
